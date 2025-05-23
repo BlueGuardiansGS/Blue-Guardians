a7623f50e7ca6adc362d6d4055a38b2daa6a19d8
Raul <rclauson141@gmail.com>
2025-05-22 21:07:42 -0300

    fixed project

--- index/js/calcula_pts.js
+++ index/js/calcula_pts.js
@@ -6,5 +6,9 @@ function calcularPontos(alternativa) {
   } else if (alternativa.value === 'alternativa_E') {
     pontuacao += 0;
   }
+  if (pontuacao >= 10){
+    pontuacao = 10;
+  }
   document.querySelector('.pontuacao').innerHTML = pontuacao;
+  document.querySelector('.pontuacao_mobile').innerHTML = pontuacao;
 }
---
+++ index/js/mostra_correta.js
@@ -0,0 +1,31 @@
+var Certa1 = document.querySelectorAll('.C1');
+var Certa2 = document.querySelectorAll('.C2');
+var Certa3 = document.querySelectorAll('.C3');
+var Certa4 = document.querySelectorAll('.C4');
+var Certa5 = document.querySelectorAll('.C5');
+
+function C1(){
+    Certa1.forEach(function(element) {
+        element.classList.add('correta');
+    });
+}
+function C2(){
+    Certa2.forEach(function(element) {
+        element.classList.add('correta');
+    });
+}
+function C3(){
+    Certa3.forEach(function(element) {
+        element.classList.add('correta');
+    });
+}
+function C4(){
+    Certa4.forEach(function(element) {
+        element.classList.add('correta');
+    });
+}
+function C5(){
+    Certa5.forEach(function(element) {
+        element.classList.add('correta');
+    });
+}
